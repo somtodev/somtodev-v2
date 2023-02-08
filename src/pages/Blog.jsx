bc2b58d697b8e70cd2ef999f6f7d744ef79386ac
ALIENROY <leroynnalue65@gmail.com>
2023-02-09 00:03:02 +0100

    + Blog Page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Blog() {
     return (
-        <>
-            <h1>Blog</h1>
-        </>
+        <main className='w-5/6 mx-auto grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-5 mt-[15vh] lg:mt-[14vh] 2xl:mt-[14vh]' data-scroll-section>
+            {/* Blog Header */}
+            <div className="flex flex-col gap-y-4 lg:gap-y-8 lg:col-span-4">
+                <div className="w-max flex items-center gap-2 text-faded hover:text-primary transition-all"><svg
+                    stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="22" width="22"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z"></path>
+                </svg>
+                    <Link className='text-sm' to={'/'}>Back</Link>
+                </div>
+                <h1 className="text-6xl lg:text-7xl">Blog</h1>
+                <p className="text-faded text-xs lg:text-sm">
+                    A portal into my thoughts, thoughts, experiences, ideas, visions,
+                    and more. What's the point of this? Well, I'm not sure, but here you go anyhow.
+                </p>
+                <br />
+                <em>Coming Soon</em>
+            </div>
+
+            {/* Blog Content */}
+            <div className="lg:col-span-8">
+            </div>
+        </main>
     )
 }
