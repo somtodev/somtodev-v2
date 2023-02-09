de083a612e48fd81748e598421bf00a8107b80db
ALIENROY <leroynnalue65@gmail.com>
2023-02-09 20:11:35 +0100

    restructured routing layout

--- src/App.js
+++ src/App.js
@@ -4,45 +4,29 @@ import {
     RouterProvider,
 } from "react-router-dom";
 
-import { useRef } from 'react'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import NotFound from './pages/NotFound';
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/blog",
-        element: <Blog />,
-    },
-    {
-        path: "/*",
-        element: <NotFound />
-    }
-]);
-
 export default function App() {
-    const containerRef = useRef(null)
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/blog",
+            element: <Blog />,
+        },
+        {
+            path: "/*",
+            element: <NotFound />
+        }
+    ]);
+
     return (
-        <LocomotiveScrollProvider
-            options={
-                {
-                    smooth: true,
-                    // ... all available Locomotive Scroll instance options 
-                }
-            }
-            watch={
-                []
-            }
-            containerRef={containerRef}
-        >
-            <RouterProvider router={router} data-scroll-container ref={containerRef} />
-        </LocomotiveScrollProvider>
+        <RouterProvider router={router} />
     )
 }
